673c061ada4d82c3b7fbf6784b03974a2ab8e3ab
Klemciak <klemcio@interia.pl>
2024-09-25 02:38:03 +0200

    added grain background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Authorizations from "./pages/authorizations/Authorizations.jsx";
+import GrainBackground from "./components/grainBackground/grainBackground.jsx";
 function App() {
   return (
     <div className="app-container">
+      <GrainBackground />
       <Routes>
         <Route path="/" element={<Authorizations />} />
       </Routes>
---
+++ src/components/grainBackground/grainBackground.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./grainBackground.scss";
+
+const GrainBackground = () => {
+  return (
+    <svg
+      viewBox="0 0 400 400"
+      xmlns="http://www.w3.org/2000/svg"
+      className="grainBackground"
+    >
+      <filter id="noiseFilter">
+        <feTurbulence
+          type="fractalNoise"
+          baseFrequency="3"
+          numOctaves="3"
+          stitchTiles="stitch"
+        >
+          <animate
+            attributeName="seed"
+            from="0"
+            to="100"
+            dur="7s"
+            repeatCount="indefinite"
+          />
+        </feTurbulence>
+      </filter>
+
+      <rect width="100%" height="100%" filter="url(#noiseFilter)" />
+    </svg>
+  );
+};
+
+export default GrainBackground;
